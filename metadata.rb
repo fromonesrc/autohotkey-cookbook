ccc4625ee50be74b8a56e9d2a763911ab97671e4
Steve McGrane <smcgrane@optoro.com>
2015-11-05 13:41:30 -0600

    Remove windows cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,3 @@ description      "Installs/Configures autohotkey"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
 supports         "windows"
-depends          "windows"
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,8 +5,6 @@
 # Copyright (C) 2012 Adam Ochonicki
 #
 
-include_recipe "windows"
-
 windows_package "autohotkey" do
   source node.autohotkey.source
   installer_type :custom
