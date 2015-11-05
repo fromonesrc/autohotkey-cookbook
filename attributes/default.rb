a48d38d489868aebb149f09726b2279ec76be588
Steve McGrane <smcgrane@optoro.com>
2015-11-05 12:58:43 -0600

    Update Source

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
-default.autohotkey.source = "http://l.autohotkey.net/AutoHotkey_L_Install.exe"
+#default.autohotkey.source = "http://l.autohotkey.net/AutoHotkey_L_Install.exe"
+default.autohotkey.source = 'http://ahkscript.org/download/ahk-install.exe'
